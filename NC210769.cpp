4710d66ac373c6cd8d78a41e5265081a769185eb
expect2004 <56511626+expect2004@users.noreply.github.com>
2020-10-17 06:54:49 +0800

    孪生的孪生素数对
    maths

---
+++ NC210769.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template < typename Tp >
+inline void read(Tp &x) {
+	x = 0; int fh = 1; char ch = 1;
+	while(ch != '-' && (ch < '0' || ch > '9')) ch = getchar();
+	if(ch == '-') fh = -1, ch = getchar();
+	while(ch >= '0' && ch <= '9') x = x * 10 + ch - '0', ch = getchar();
+	x *= fh;
+}
+
+template < typename Tp >
+inline void biread(Tp &x) {
+	x = 0; int fh = 1; char ch = 1;
+	while(ch != '-' && (ch < '0' || ch > '9')) ch = getchar();
+	if(ch == '-') fh = -1, ch = getchar();
+	while(ch >= '0' && ch <= '9') x = x * 2 + ch - '0', ch = getchar();
+	x *= fh;
+}
+
+long long n;
+
+inline void Init(void) {
+	cin >> n;
+}
+
+inline void Work(void) {
+	if(n < 7) {
+		puts("0");
+	}
+	else {
+		printf("1\n3 5 7\n");
+	}
+}
+
+signed main(void) {
+	Init();
+	Work();
+	return 0;
+}
+
