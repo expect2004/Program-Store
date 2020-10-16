3eee0f28c7868446ba716ee234046dfb9d1a497b
expect2004 <56511626+expect2004@users.noreply.github.com>
2020-10-17 07:41:33 +0800

    [MtOI2019]永夜的报应
    maths, xor

---
+++ LG5514.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template < typename Tp >
+inline void read(Tp &x) {
+	x = 0; int fh = 1; char ch = 1;
+	while(ch != '-' && (ch < '0' || ch > '9')) ch = getchar();
+	if(ch == '-') fh = -1, ch = getchar();
+	while(ch >= '0' && ch <= '9') x = x * 10 + ch - '0', ch = getchar();
+	x *= fh;
+}
+
+template < typename Tp >
+inline void biread(Tp &x) {
+	x = 0; int fh = 1; char ch = 1;
+	while(ch != '-' && (ch < '0' || ch > '9')) ch = getchar();
+	if(ch == '-') fh = -1, ch = getchar();
+	while(ch >= '0' && ch <= '9') x = x * 2 + ch - '0', ch = getchar();
+	x *= fh;
+}
+
+const int maxn = 1000000 + 7;
+
+int n;
+long long x, ans;
+
+inline void Init(void) {
+	read(n);
+	for(int i = 1; i <= n; i++) {
+		read(x); ans ^= x;
+	}
+	printf("%lld\n", ans);
+}
+
+inline void Work(void) {
+
+}
+
+signed main(void) {
+	Init();
+	Work();
+	return 0;
+}
+
